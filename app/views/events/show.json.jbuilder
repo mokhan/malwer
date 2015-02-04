fe7d2f8b8d806c65899c42d215cdd1578b3a108d
mo khan <mo@mokhan.ca>
2015-02-03 20:26:19 -0700

    remove extra stuff.

--- app/views/events/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @event, :id, :name, :data, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :events
+  resources :events, only: [:index, :new, :create, :destroy]
   root 'events#index'
 end
