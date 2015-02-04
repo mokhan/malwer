4c5494ec4a3cb751e50a307b35c8d067ecc6d5be
mo khan <mo@mokhan.ca>
2015-02-03 22:43:28 -0700

    attempt to lookup disposition for cloud query.

--- app/workers/cloud_queries.rb
+++ app/workers/cloud_queries.rb
@@ -6,6 +6,14 @@ class CloudQueries
 
   def work(json)
     logger.info "Query for: #{json.inspect}"
+    attributes = JSON.parse(json)
+    fingerprint = attributes["fingerprint"]
+    disposition = Disposition.find_by(fingerprint: fingerprint)
+    if disposition.present?
+      logger.info("#{disposition.state} disposition for: #{fingerprint}")
+    else
+      logger.info("Unknown disposition for: #{fingerprint}")
+    end
 
     ack!
   end
