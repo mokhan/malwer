feaf5cfae0f948ceebae4d1ff2c80d84cd76cae0
mo khan <mo@mokhan.ca>
2015-02-21 12:41:44 -0700

    authorize all requests otherwise redirect to login page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   #protect_from_forgery with: :exception
   protect_from_forgery with: :null_session
+  before_action :authorize!
+
+  private
+
+  def authorize!
+    redirect_to new_session_path if current_user.nil?
+  end
+
+  def current_user
+    return nil if session[:x].blank?
+    @current_user ||= User.find(session[:x])
+  end
 end
