29e46f29102844a9a9ab77d17d2b9363ac78ec70
mo khan <mo@mokhan.ca>
2015-03-04 20:58:15 -0700

    correct the logout specs.

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -29,10 +29,21 @@ RSpec.describe SessionsController, type: :controller do
     end
   end
 
-  describe "GET #destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
+  describe "#destroy" do
+    let(:user) { create(:user) }
+
+    before :each do
+      session[:x] = user.id
+    end
+
+    it "redirects to the login page" do
+      delete :destroy, id: 'me'
+      expect(response).to redirect_to(new_session_path)
+    end
+
+    it "resets the session" do
+      delete :destroy, id: 'me'
+      expect(session[:x]).to be_nil
     end
   end
 end
