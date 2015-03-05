266e990e560f32d26f0ba1fb59a062cf42a5a138
mo khan <mo@mokhan.ca>
2015-03-04 22:08:35 -0700

    add a pending spec.

---
+++ spec/controllers/agents/events_controller_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+module Agents
+  RSpec.describe EventsController do
+    describe "#index" do
+      pending "only loads events for agents that belong to the logged in user"
+    end
+  end
+end
